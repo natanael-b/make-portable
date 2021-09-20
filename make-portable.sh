f7bca292211d4931d987f95f3e8500bb4c7caae1
Natanael <natanaelgplay@gmail.com>
2021-09-20 20:48:54 -0300

    Update make-portable.sh

--- make-portable.sh
+++ make-portable.sh
@@ -2,6 +2,182 @@
 
 export HERE="$(dirname "$(readlink -f "${0}")")"
 
+for arg in "${@}"; do
+  echo "${arg}" | grep -q ^"--appdir=" && {
+    appdir=$(echo "${arg}" | cut -c 10-).AppDir
+    shift
+  }
+  echo "${arg}" | grep -q ^"--autoclose=" && {
+    timer=$(echo "${arg}" | cut -c 13-)
+    shift
+  }
+  echo "${arg}" | grep -q ^"--icon=" && {
+    icon_file=$(echo "${arg}" | cut -c 8-)
+    icon_file=$(readlink -f "${icon_file}")
+    [ ! -f "${icon_file}" ] && {
+      echo "Warning: File ${icon_file} not found"
+      unset icon_file
+    }
+    shift
+  }
+  echo "${arg}" | grep -q ^"--desktop=" && {
+    desktop_file=$(echo "${arg}" | cut -c 11-)
+    share_dirs=($(echo ${XDG_DATA_DIRS} | tr ':' '\n'))
+    for dir in "${share_dirs[@]}"; do
+      dir="${dir}/applications"
+      [ -f "${dir}/${desktop_file}.desktop" ] && {
+        desktop_file=$(echo "${dir}/${desktop_file}.desktop")
+        break
+      }
+    done
+    [ ! -f "${desktop_file}" ] && {
+      echo "Warning: Desktop launcher ${icon_file} not found"
+      unset desktop_file
+    }
+    shift
+  }
+  echo "${arg}" | grep -q ^"--minimal-apprun"$ && {
+    overwrite_apprun="true"
+    shift
+  }
+done
+
+[ "${timer}"  = "" ] && export timer=25
+[ "${appdir}" = "" ] && export appdir=$(basename "${1}").AppDir
+
+main_exe_name="${1}"
+main_exe_path=$(readlink -f "${1}")
+
+[ ! -f "${main_exe_path}" ] && {
+  main_exe_path=$(which "${1}")
+}
+
+shift
+
+[ ! -f "${main_exe_path}" ] && {
+  echo "Error: The executable '${main_exe_name}' doesn't exist!"
+  exit 1
+}
+
+
+echo "Creating portable environment..."
+
+mkdir -p "${appdir}/lib"
+
+cp "${HERE}/libunion.so" "${appdir}/lib"
+cp "${HERE}/libexec.so"  "${appdir}/lib"
+
+chmod a-x "${appdir}/lib/libexec.so"
+chmod a-x "${appdir}/lib/libunion.so"
+
+cd "${appdir}"
+
+echo "Creating AppRun.."
+
+full_path="\${HERE}/${main_exe_path}"
+
+(
+  echo -E '#!/usr/bin/env bash'
+  echo -E 'export HERE="$(dirname "$(readlink -f "${0}")")"'
+  echo -E 'export SYSTEM_UNION_PRELOAD=""'
+  echo -E "\"${full_path}\" \"\${@}\""
+) > "AppRun"
+
+echo "Fetching accessed files..."
+
+timeout ${timer} strace -f -e file -o accessed.list "${main_exe_path}" ${@}
+sed -i 's/^[0-9]*  //' accessed.list
+
+executables=($(cat accessed.list | grep -Ev "ENOEXEC|ENOENT" | grep ^exec | cut -d\" -f2))
+
+sed -i '/NOENT/d;s|AT_FDCWD, ||g;s|^.*("||g;s|", .*||g;s|/|§|g' accessed.list
+
+
+sed -i -n '/^§etc§fonts\|^§usr\|^§nix§\|^§lib\|^§bin\|^§sbin\|^§opt/p'  accessed.list
+sed -i 's|§|/|g' accessed.list
+
+files=($(cat accessed.list))
+
+rm accessed.list
+
+# Remove Video driver related libs
+[ -f ./"lib/libGLX.so.0" ]         && rm ./"lib/libGLX.so.0"
+[ -f ./"lib/libGL.so.1" ]          && rm ./"lib/libGL.so.1"
+[ -f ./"lib/libGLdispatch.so.0" ]  && rm ./"lib/libGLdispatch.so.0"
+[ -f ./"lib/libGLX_mesa.so.0" ]    && rm ./"lib/libGLX_mesa.so.0"
+drivers=$(find . | grep "_dri\.so"$)
+[ ! -z "${drivers}" ] && rm ${drivers}
+
+echo "Getting system libraries..."
+
+system_libs=$(ldconfig -iNv 2> /dev/null | cut -c 2- | cut -d' ' -f1)
+
+echo "Copying files from system to portable environment..."
+
+for file in "${files[@]}"; do 
+  name=$(basename "${file}")
+  [ -f "${file}" ] && { 
+    echo "${system_libs}" | grep -q "${name}" && {
+      cp --no-clobber "${file}" ./lib
+    } || {
+      cp --parent --no-clobber "${file}" .
+    }
+  }
+done
+
+cp "/lib64/ld-linux-x86-64.so.2" ./"lib"
+
+# Support for flatpak
+
+[ -f "/app/manifest.json" ] && {
+  echo "Running inside flatpak, bundling /app..."
+  cp -r --parent "/app/" .
+}
+
+executables=$(find . -type f -executable)
+
+LIB_PATHS=$(find . | grep ".so"   | sed 's|^|dirname |g' | sh | sort | uniq | sed 's|^.|${HERE}|g;s|$|:|g' | tr -d '\n')
+EXE_PATHS=$(echo "${executables}" | sed 's|^|dirname |g' | sh | sort | uniq | sed 's|^.|${HERE}|g;s|$|:|g' | tr -d '\n')
+
+
+potential_libs=($(echo "${executables}"))
+unset executables
+
+for potential_lib in ${potential_libs[@]}; do
+  base=$(basename ${potential_lib})
+  
+  echo "${system_libs}" | grep -q ^"${base}"$ || {
+    executables+=("${potential_lib}")
+  }
+done
+
+
+echo "Bundling glib schemas..."
+mkdir -p "usr/share/glib-2.0/schemas/"
+cp --no-clobber "/usr/share/glib-2.0/schemas/gschemas.compiled" "usr/share/glib-2.0/schemas/"
+
+echo "${system_libs}" | grep -q ^"libgdk_pixbuf-2.0.so.0" && {
+  echo "Importing gdk-pixbuff..."
+  cp -r --parent /usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0/*/ . 
+  
+  pixbux_loaders_cache_file="\${HERE}"$(find . -type f -name "loaders.cache" | grep "gdk-pixbuf-2.0" | cut -c 2-)
+  pixbux_loaders_dir=$(dirname "${pixbux_loaders_cache_file}")
+}
+
+echo "Importing girepository-1.0..."
+mkdir -p ./AppImage/girepository-1.0
+typelibs=($(find . -type f -name *.typelib))
+for typelib in "${typelibs[@]}"; do
+  mv "${typelib}" ./AppImage/girepository-1.0
+done
+
+echo "Creating launcher..."
+
+cat > launcher <<\EOF
+#!/usr/bin/env bash
+
+export HERE="$(dirname "$(readlink -f "${0}")")"
+
 [ ! "${SYSTEM_UNION_PRELOAD}" = "${HERE}/lib/libunion.so:${HERE}/lib/libexec.so" ] && {
   # Backup environment variables
 
@@ -79,4 +255,49 @@ unset LD_LIBRARY_PATH
 }
 
 "${HERE}/lib/ld-linux-x86-64.so.2" --inhibit-cache --library-path "${LIB_PATH}" "${EXE_PATH}" "${@}"
+
+EOF
+
+chmod a+x "launcher"
+chmod a+x "AppRun"
+
+echo "Wrapping executables..."
+
+for executable in "${executables[@]}";do
+  executable=$(echo -nE "${executable}" | cut -c 2-)
+    
+  echo "Wrapping '${executable}'..."
+
+  wrapped_path="\${HERE}${executable}.wrapped"
+  executable="$(pwd)${executable}"
+  
+  mv "${executable}" "${executable}.wrapped"
+  
+  magic=$(head -n1 "${executable}.wrapped" | cut -c 1-2)
+  
+  [ "${magic}" = "#!" ] && {
+    echo "TODO: Suporte a scripts"
+  } || {
+    (
+      echo -E '#!/usr/bin/env bash'
+      echo -E "export EXE_PATH=\"${wrapped_path}\""
+      echo -E "\"\${HERE}/launcher\" \"\${@}\""
+    ) > "${executable}"
+  }
+  
+  chmod +x "${executable}"
+  
+done
+
+[ -f "${icon_file}" ] && {
+  cp "${icon_file}" .
+}
+
+[ -f "${desktop_file}" ] && {
+  cp "${desktop_file}" .
+  desktop_file=$(basename "${desktop_file}")
+  sed -i -e "s|DBusActivatable|X-DBusActivatable|g;s|Keywords|X-Keywords|g" "${desktop_file}"
+}
+
+[ -d ./"usr/share/fonts" ] && rm -rf ./"usr/share/fonts"
 
