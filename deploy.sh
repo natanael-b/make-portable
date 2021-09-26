3d300e56e208b2bfb4140a2736bcd0671adbcac8
Natanael <natanaelgplay@gmail.com>
2021-09-26 17:26:35 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
+(
+  cd preloads
+  make
+)
 
-mkdir -p make-portable.AppDir
-cd make-portable.AppDir
+(
+  mkdir -p make-portable.AppDir
+  cd make-portable.AppDir
 
-cp ../*.so ../make-portable* .
+  cp ../preloads/*.so ../make-portable* .
 
-chmod +x make-portable.sh
-ln -s make-portable.sh AppRun
+  chmod +x make-portable.sh
+  ln -s make-portable.sh AppRun
 
-./AppRun --appdir=Strace --minimal-apprun strace --help
+  ./AppRun --appdir=Strace --minimal-apprun strace --help
 
-cd ..
+)
 
 wget -q "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
-
 chmod +x appimagetool-x86_64.AppImage
-
 ARCH=x86_64 ./appimagetool-x86_64.AppImage --appimage-extract-and-run make-portable.AppDir
 
 mv make-portable-x86_64.AppImage make-portable
-
-rm -rf make-portable.AppDir appimagetool-x86_64.AppImage
+rm -rfv make-portable.AppDir appimagetool-x86_64.AppImage
